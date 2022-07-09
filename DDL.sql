b52ec809fa5c9ff448a19ae3a47e12581f86edf2
Chris Felt <feltc@oregonstate.edu>
2022-07-08 18:35:25 -0600

    removed MySQL Workbench SET commands

--- DDL.sql
+++ DDL.sql
@@ -4,10 +4,6 @@
 -- Members: Robert Behring and Christopher Felt
 -- Modified from MySQL Workbench Forward Engineering
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-
 -- -----------------------------------------------------
 -- Table Users
 -- -----------------------------------------------------
@@ -101,8 +97,3 @@ CREATE TABLE IF NOT EXISTS Shipments_has_Rocks (
     REFERENCES Rocks (rockID)
     ON DELETE RESTRICT
     ON UPDATE NO ACTION);
-
-
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
