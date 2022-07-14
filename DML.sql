aa0f345652500eee1c276464b482a4009a4dc66d
Chris Felt <feltc@oregonstate.edu>
2022-07-13 22:28:15 -0600

    DML update: added queries for Rocks (CR no UD)

--- DML.sql
+++ DML.sql
@@ -8,19 +8,22 @@
 -- Users Data Manipulation Queries
 -- -----------------------------------------------------
 
--- get all Users for the browse Users table
-SELECT userID, name, address, specialization, bio
-    FROM Users
-
+-- CREATE
 -- add a new User
 INSERT INTO Users (name, address, specialization, bio)
     VALUES (:nameInput, : addressInput, :specializationInput, :bioInput)
 
+-- READ
+-- get all Users for the browse Users table
+SELECT userID, name, address, specialization, bio
+    FROM Users
+
+-- UPDATE
 -- update a User's data using form input data
 -- first, get User
 SELECT userID, name, address, specialization, bio
     FROM Users
-    WHERE userID = :userID_selected_in_form -- pulled from onclick event when edit link is clicked
+    WHERE userID = :userID_selected_in_form -- pulled from onclick event when edit User link is clicked
 -- then, update User
 UPDATE Users
     SET name = :nameInput, address = :addressInput, specialization = :specializationInput, bio = :bioInput
@@ -31,6 +34,17 @@ UPDATE Users
 -- Rocks Data Manipulation Queries
 -- -----------------------------------------------------
 
+-- CREATE
+-- add a new Rock
+INSERT INTO Rocks (userID, name, geoOrigin, type, description, chemicalComp)
+    VALUES (SELECT userID FROM Users WHERE name = :name_from_dropdown_input,　-- what about Users with same name?
+    name = :nameInput, geoOrigin = :geoOriginInput, type = :typeInput, chemicalComp = :chemicalCompInput)
+
+-- READ
+-- get all Rocks for the browse Rocks table
+SELECT rockID, SELECT name FROM Users INNER JOIN Rocks ON Users.userID = Rocks.userID,
+    name, geoOrigin, type, description, chemicalComp
+    FROM Rocks
 
 
 -- -----------------------------------------------------
