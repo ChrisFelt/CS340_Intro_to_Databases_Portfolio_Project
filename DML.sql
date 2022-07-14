d3f130c6663d3c608198560331bdb7f1e5a54285
Chris Felt <feltc@oregonstate.edu>
2022-07-13 18:15:52 -0600

    DML update: added queries for Users (CRU, no D)

--- DML.sql
+++ DML.sql
@@ -8,6 +8,23 @@
 -- Users Data Manipulation Queries
 -- -----------------------------------------------------
 
+-- get all Users for the browse Users table
+SELECT userID, name, address, specialization, bio
+    FROM Users
+
+-- add a new User
+INSERT INTO Users (name, address, specialization, bio)
+    VALUES (:nameInput, : addressInput, :specializationInput, :bioInput)
+
+-- update a User's data using form input data
+-- first, get User
+SELECT userID, name, address, specialization, bio
+    FROM Users
+    WHERE userID = :userID_selected_in_form -- pulled from onclick event when edit link is clicked
+-- then, update User
+UPDATE Users
+    SET name = :nameInput, address = :addressInput, specialization = :specializationInput, bio = :bioInput
+    WHERE userID = :userID_selected_in_form
 
 
 -- -----------------------------------------------------
